606912868bef5a2fc0e723eb5eb8ecd39f41a7d9
koudenpa <koudenpa@hotmail.com>
2020-06-27 16:03:55 +0900

    SaaSで焼き肉屋をレコメンド。

--- HackAzureYakinikuApp/Models/YakinikuViewModel.cs
+++ HackAzureYakinikuApp/Models/YakinikuViewModel.cs
@@ -8,5 +8,20 @@ namespace HackAzureYakinikuApp.Models
     public class YakinikuViewModel
     {
         public long Balance { get; set; }
+
+        public long PriceH
+        {
+            get
+            {
+                return Balance;
+            }
+        }
+        public long PriceL
+        {
+            get
+            {
+                return (long)(Balance * 0.7);
+            }
+        }
     }
 }
