e7fb00dddec8ad84a2fb554f9c5ace0fa38ae85b
aetos <aetos@aerie.jp>
2020-06-27 14:25:26 +0900

    stripe startup

--- HackAzureYakinikuApp/Startup.cs
+++ HackAzureYakinikuApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Stripe;
+
 namespace HackAzureYakinikuApp
 {
     public class Startup
@@ -25,6 +27,15 @@ namespace HackAzureYakinikuApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            ConfigureStripe();
+        }
+
+        private void ConfigureStripe()
+        {
+            string secretKey = this.Configuration["STRIPE_SECRET_KEY"];
+
+            StripeConfiguration.ApiKey = secretKey;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +48,11 @@ namespace HackAzureYakinikuApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
